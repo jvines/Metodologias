7d203e18f8ce6cca77983867bb63f6ad829374b4
jvines <jose.vines@ug.uchile.cl>
2017-03-30 00:59:20 -0300

    Now implements IType, added weakAgainst* methods for double dispatch.

--- code/cc3002_homework/src/pokemon/AbstractType.java
+++ code/cc3002_homework/src/pokemon/AbstractType.java
@@ -1,5 +1,53 @@
 package pokemon;
 
-public abstract class AbstractType {
+public abstract class AbstractType implements IType {
+
+  @Override
+  public void weakAgainstGrass(IType o) {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public void weakAgainstFire(IType o) {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public void weakAgainstWater(IType o) {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public void weakAgainstElectric(IType o) {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public void weakAgainstGround(IType o) {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public void weakAgainstPsychic(IType o) {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public void weakAgainstFighting(IType o) {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public void weakAgainstNormal(IType o) {
+    // TODO Auto-generated method stub
+
+  }
 
 }
