9f5a77b4c531ce079d9a6f8ea2f715228c4db865
jvines <jose.vines@ug.uchile.cl>
2017-03-30 00:58:38 -0300

    Added abstract Pokemon class.

--- code/cc3002_homework/src/pokemon/AbstractPokemon.java
+++ code/cc3002_homework/src/pokemon/AbstractPokemon.java
@@ -1,11 +1,23 @@
 package pokemon;
 
-public abstract class AbstractPokemon {
+public abstract class AbstractPokemon implements IType, IPokemon {
 
   protected int hp;
-  protected Type type;
-  public void attack(){
+  protected IType type;
+  protected int move_power;
+
+  public void attack() {
     // placeholder
   }
+  public int getHp() {
+    return hp;
+  }
   
+  public IType getType() {
+    return type;
+  }
+  
+  public void decreaseHp(int x) {
+    hp -= x;
+  }
 }
